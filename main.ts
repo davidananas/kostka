7b7e92c5bcd96f905e3ea61eeb0a61a8e35dffe7
davidananas <116786922+davidananas@users.noreply.github.com>
2022-12-24 11:28:25 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
+function pokyn () {
+    cislo = randint(0, 5)
+    rolldice()
+    if (cislo == 0) {
+        music.playTone(262, music.beat(BeatFraction.Whole))
+    } else if (cislo == 1) {
+        music.playTone(294, music.beat(BeatFraction.Whole))
+    } else if (cislo == 2) {
+        music.playTone(330, music.beat(BeatFraction.Whole))
+    } else if (cislo == 3) {
+        music.playTone(349, music.beat(BeatFraction.Whole))
+    } else if (cislo == 4) {
+        music.playTone(392, music.beat(BeatFraction.Whole))
+    } else if (cislo == 5) {
+        music.playTone(440, music.beat(BeatFraction.Whole))
+        music.playTone(523, music.beat(BeatFraction.Whole))
+        music.playTone(330, music.beat(BeatFraction.Whole))
+    }
+}
 function setdicepatterns () {
     list = [
     images.createImage(`
@@ -45,11 +64,16 @@ function setdicepatterns () {
     ]
 }
 input.onGesture(Gesture.Shake, function () {
-    rolldice()
+    pokyn()
 })
 function rolldice () {
-    list[randint(0, 5)].showImage(0)
+    list[cislo].showImage(0)
 }
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    pokyn()
+})
 let list: Image[] = []
+let cislo = 0
+cislo = randint(0, 5)
 setdicepatterns()
 rolldice()
